be8d463e89968a9f3c0aebc4bb6424fd1b862894
abdulrauf <abdulroufsidhu@gmail.com>
2024-09-17 22:33:59 +0500

    auth apis written

--- projects/orgolink-datalayer/src/lib/datalayers/auth.ts
+++ projects/orgolink-datalayer/src/lib/datalayers/auth.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { LoginRes, User, LoginReq } from "../models/core-models";
+import { LoginRes, User, LoginReq, ResponseModel } from "../models/core-models";
 import { Observable } from "rxjs";
 import { inject } from "@angular/core";
 enum SessionStorageKeys {
@@ -29,31 +29,50 @@ export class AuthDatalayer {
   private BASE_URL = "http://localhost:8080/";
   private httpClient: HttpClient = inject(HttpClient);
 
+  private createFullUrl = (url: string) => `${this.BASE_URL}url`;
+
   constructor(base_url?: string) {
     if (!!base_url) this.BASE_URL = base_url;
   }
 
-  login(request: LoginReq): Observable<LoginRes> {
-    return this.httpClient.post<LoginRes>(
-      `${this.BASE_URL}/api/users/login`,
+  login(request: LoginReq): Observable<ResponseModel<LoginRes>> {
+    return this.httpClient.post<ResponseModel<LoginRes>>(
+      this.createFullUrl(`/api/users/login`),
       request,
       {},
     );
+  }
+
+  updateAuthCache(loginResponse: LoginRes) {
+    setToken(loginResponse.token);
+    setUser(loginResponse.user);
   }
 
   signup(request: User) {
-    return this.httpClient.post<LoginRes>(
-      `${this.BASE_URL}/api/users/sign-up`,
+    return this.httpClient.post<ResponseModel<LoginRes>>(
+      this.createFullUrl(`/api/users/sign-up`),
       request,
       {},
     );
   }
 
-  changePassword(oldPassword: string, newPassword: string) {
+  changePassword(old_password: string, new_password: string) {
     const u = getUser();
-    return this.httpClient.patch(
-      `${this.BASE_URL}/api/users/update-password/${u?.id}`,
-      {},
+    return this.httpClient.patch<ResponseModel<User>>(
+      this.createFullUrl(`/api/users/update-password`),
+      { old_password, new_password },
+      {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      },
+    );
+  }
+
+  updateUser(user: User) {
+    return this.httpClient.patch<ResponseModel<User>>(
+      this.createFullUrl("/api/users/update-user"),
+      user,
       {
         headers: {
           Authorization: `Bearer ${getToken()}`,
